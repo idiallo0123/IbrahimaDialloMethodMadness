27cb5165d22f7c0eeccac09cdfe058d41fa61b73
IDiallo <idiallo0123@bths.edu>
2018-10-03 08:38:05 -0400

    Classwork 10/3/18

--- src/IbrahimaDiallo/IDialloLib.java
+++ src/IbrahimaDiallo/IDialloLib.java
@@ -1,8 +1,31 @@
 package IbrahimaDiallo;
 
-public class IDialloLib {
-    public static String newProject(String tesT) {
-        tesT = "Hi";
-        //System.out.println(tesT);
+public class IDialloLib
+{
+    public static boolean isPalindrome(String input)
+    {
+        String sub1 = "";
+        String sub2 = "";
+
+        if((input.length()%2) == 0)  //Even number or char
+        {
+            sub1 = input.substring(0, (input.length()/2));
+            sub2 = input.substring((input.length()/2), input.length());
+        }
+        else
+        {
+            sub1 = input.substring(0, (input.length()/2)); //That 0.5 will round down
+            sub2 = input.substring((input.length()/2), (input.length()+1));
+        }
+
+        for(int i = 0; i < sub2.length(); i++)
+        {
+            sub2 = sub2.substring(sub2.length()) + sub2.substring(0, (sub2.length()+1));
+            //This loop will reverse the order of char in sub2
+        }
+
+        System.out.println(sub1);
+        System.out.println(sub2);
+        return(sub1.equals(sub2));
     }
 }
--- src/IbrahimaDiallo/Main.java
+++ src/IbrahimaDiallo/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(IDialloLib.newProject(String tesT));
+        System.out.println(IDialloLib.isPalindrome("racecar"));
     }
 }
 //Check to see if I can update to github
